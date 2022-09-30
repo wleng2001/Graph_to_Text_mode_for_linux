67deac3e0273bbb659602627027a5cc1ad36b4d0
wleng2001 <112073988+wleng2001@users.noreply.github.com>
2022-09-30 15:25:28 +0200

    repair nano mistake

--- edit_grub.py
+++ edit_grub.py
@@ -6,7 +6,6 @@ import os
 
 text_to_change=['GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"','GRUB_CMDLINE_LINUX_DEFAULT="quiet"','GRUB_CMDLINE_LINUX=""','#GRUB_TERMINAL="console"']
 changed_text=['#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"','#GRUB_CMDLINE_LINUX_DEFAULT="quiet"','GRUB_CMDLINE_LINUX="text"','GRUB_TERMINAL="console"']
-
 file_name="grub.backup"
 text_file_name="grub.backuptext"
 graph_file_name="grub.backupgraph"
@@ -65,10 +64,10 @@ default_info=open(default_info_name, 'w')
 
 #add analize default grub function
 for i in changed_text:
-    if !i.startswith("#")==False:
+    if i.startswith("#")==False:
         i="#"+i
     find_text_in_list(i, grub_text)
- 
+
 if find_text_in_list(text_to_change[1], grub_text):
 	print("You're in graphical mode.")
 	text_mode=False
